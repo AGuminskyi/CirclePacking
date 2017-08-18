33cb9cf7b95197293b685d77faae0f8fbc1debaa
AGuminskyi <cubrubarts@gmail.com>
2017-08-18 15:49:38 +0300

    Add cuboidButton

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,6 +2,8 @@ package com.idapgroup.artemhuminkiy.circlepacking
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        firstButton.setOnClickListener { Toast.makeText(this, "BUTTON 1", Toast.LENGTH_SHORT).show() }
+        secondButton.setOnClickListener { Toast.makeText(this, "BUTTON 2", Toast.LENGTH_SHORT).show() }
+        thirdButton.setOnClickListener { Toast.makeText(this, "BUTTON 3", Toast.LENGTH_SHORT).show() }
+
+        firstButton.circle_border_color =
+
 //        var categ : Category(this, #f57f17, one)
-
-
-
 //        val circle1 : Circle = Circle(5.3F)
 //        val circle2 : Circle = Circle(4.2F)
 //
